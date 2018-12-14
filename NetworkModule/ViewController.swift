424dcab2f64ff16ebd524afbaa5bcea453ea2a46
1002659 <baw0803@sk.com>
2018-12-14 11:11:07 +0900

    App init

---
+++ NetworkModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkModule
+//
+//  Created by wooky83 on 14/12/2018.
+//  Copyright © 2018 wooky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
