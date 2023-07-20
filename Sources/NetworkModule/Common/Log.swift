bfc4e6d6b71e02ff574813aff7f1cc6dadf88105
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-07-20 10:00:56 +0900

    Revise

--- Sources/NetworkModule/Common/Log.swift
+++ Sources/NetworkModule/Common/Log.swift
@@ -1,54 +1,21 @@
 import Foundation
 import OSLog
 
-// MARK: - Log
-extension OSLog {
-    static let subsystem = Bundle.main.bundleIdentifier!
-//    static let network = OSLog(subsystem: subsystem, category: "Network")
-//    static let debug = OSLog(subsystem: subsystem, category: "Debug")
-//    static let info = OSLog(subsystem: subsystem, category: "Info")
-//    static let error = OSLog(subsystem: subsystem, category: "Error")
-}
-
 public enum Log {
-    fileprivate enum Level {
-        case debug
-        case info
-        case network
-        case error
-        case custom(categoryName: String)
 
-        fileprivate var category: String {
-            switch self {
-            case .debug:
-                return "Debug"
-            case .info:
-                return "Info"
-            case .network:
-                return "Network"
-            case .error:
-                return "Error"
-            case .custom(let categoryName):
-                return categoryName
-            }
-        }
-    }
-
-    static private func log(_ message: [String], level: Level, file: String, fnc: String, line: Int) {
-        let logger = Logger(subsystem: OSLog.subsystem, category: level.category)
+    private static let subsystem = Bundle.main.bundleIdentifier ?? ""
+    /// OSLogMessage
+    /// argumentString: @autoclosure @escaping () -> String,
+    /// format: OSLogIntegerFormatting = .decimal,
+    /// align: OSLogStringAlignment = .none,
+    /// privacy: OSLogPrivacy = .auto,
+    /// attributes: String
+    static private func log(_ message: [String], level: OSLogType, category: String? = nil, file: String, fnc: String, line: Int) {
+        let logger = Logger(subsystem: subsystem, category: category ?? level.description)
         let className = file.components(separatedBy: "/").last?.components(separatedBy: ".").first ?? ""
         let function = fnc.unicodeScalars.filter { CharacterSet.alphanumerics.contains($0) }.map { String($0) }.joined()
         let logMessage = "[\(className):\(function):\(line)] \(message.joined(separator: ", "))"
-        switch level {
-        case .debug, .custom:
-            logger.debug("\(logMessage, privacy: .public)")
-        case .info:
-            logger.info("\(logMessage, privacy: .public)")
-        case .network:
-            logger.log("\(logMessage, privacy: .public)")
-        case .error:
-            logger.error("\(logMessage, privacy: .public)")
-        }
+        logger.log(level: level, "\(logMessage)")
     }
 }
 
@@ -63,7 +30,7 @@ public extension Log {
     }
 
     static func network(_ messages: String..., file: String = #file, fnc: String = #function, line: (Int)=#line) {
-        log(messages, level: .network, file: file, fnc: fnc, line: line)
+        log(messages, level: .info, category: "Network", file: file, fnc: fnc, line: line)
     }
 
     static func error(_ messages: String..., file: String = #file, fnc: String = #function, line: (Int)=#line) {
@@ -71,7 +38,26 @@ public extension Log {
     }
 
     static func custom(_ messages: String..., category: String, file: String = #file, fnc: String = #function, line: (Int)=#line) {
-        log(messages, level: .custom(categoryName: category), file: file, fnc: fnc, line: line)
+        log(messages, level: .debug, category: category, file: file, fnc: fnc, line: line)
     }
 
+}
+
+extension OSLogType {
+    var description: String {
+        switch self {
+        case .default:
+            return "Default"
+        case .info:
+            return "Info"
+        case .debug:
+            return "Debug"
+        case .error:
+            return "Error"
+        case .fault:
+            return "Fault"
+        default:
+            return "Unknown"
+        }
+    }
 }
