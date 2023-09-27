0dfade61085e63a7b6e4d56e4b1c880cd4f263b3
Kwon Sung Wook[ 권성욱 ] <baw0803@linecorp.com>
2023-09-27 14:33:38 +0900

    Success

--- Tests/NetworkModuleTests/TestHelper/MockService.swift
+++ Tests/NetworkModuleTests/TestHelper/MockService.swift
@@ -1,32 +1,24 @@
 import Foundation
 import Combine
 import PromiseKit
-import RxAlamofire
-import RxSwift
 @testable import AlamofireNetwork
 @testable import URLSessionNetwork
-@testable import RxNetwork
 
 struct MockService {
 
     public static func jsonplaceholderUser(_ param: [String: String]? = nil) -> AnyPublisher<PostModel, Error> {
-        URLSessionNetwork.Remote<PostModel>(url: "http://localhost:1234/api/v2/users", parameter: param)
+        Remote<PostModel>(url: "http://localhost:1234/api/v2/users", parameter: param)
             .asPublisher()
     }
 
     public static func jsonplaceholderUserFail() -> AnyPublisher<FailModel, Error> {
-        URLSessionNetwork.Remote<FailModel>(url: "http://localhost:1234/api/v2/users")
+        Remote<FailModel>(url: "http://localhost:1234/api/v2/users")
             .asPublisher()
     }
 
     public static func jsonplaceholderUser(_ param: [String: String]? = nil) -> Promise<PostModel> {
         NetworkTask<PostModel>(parameter: param).requestNetworkConnection("http://localhost:1234/api/v2/users")
     }
-
-    public static func rxAlamofireSuccess() -> Observable<PostModel> {
-        RxNetwork.Remote<PostModel>(url: "http://localhost:1234/api/v2/users").asObservable()
-    }
-
 }
 
 struct PostModel: Codable {
